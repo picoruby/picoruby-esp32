2442df2e124233e120ba73f1b050105a729ad459
yuuu <okazaki@fusic.co.jp>
2025-11-24 21:35:11 +0900

    Add picoruby-esp32 mrbgem which has WiFi class.

--- picoruby-esp32.c
+++ picoruby-esp32.c
@@ -1,5 +1,5 @@
 #include <inttypes.h>
-
+#include <nvs_flash.h>
 #include "picoruby.h"
 
 #include <mrubyc.h>
@@ -11,8 +11,21 @@
 
 static uint8_t heap_pool[HEAP_SIZE];
 
-void picoruby_esp32(void)
+void
+initialize_nvs(void)
 {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+}
+
+void
+picoruby_esp32(void)
+{
+  initialize_nvs();
   mrbc_init(heap_pool, HEAP_SIZE);
 
   mrbc_tcb *main_tcb = mrbc_create_task(main_task, 0);
