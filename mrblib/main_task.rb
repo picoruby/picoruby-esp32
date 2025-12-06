c4c167a505a87379df4443e64d7102f5227548a2
yuuu <okazaki@fusic.co.jp>
2025-12-06 16:09:54 +0900

    Connect to Wi-Fi at startup.

--- mrblib/main_task.rb
+++ mrblib/main_task.rb
@@ -1,9 +1,59 @@
 require 'machine'
 require "watchdog"
+require 'env'
+require 'yaml'
+require "mbedtls"
+require "base64"
+require 'esp32'
 Watchdog.disable
 require "shell"
 STDIN = IO.new
 STDOUT = IO.new
+
+def decrypt_proc(decoded_password)
+  cipher = MbedTLS::Cipher.new("AES-256-CBC")
+  cipher.decrypt
+  key_len = cipher.key_len
+  iv_len = cipher.iv_len
+  unique_id = Machine.unique_id
+  len = unique_id.length
+  cipher.key = (unique_id * ((key_len / len + 1) * len))[0, key_len].to_s
+  cipher.iv = (unique_id * ((iv_len / len + 1) * len))[0, iv_len].to_s
+  # Based on successful debug run: CBC mode has empty tag, so entire data is ciphertext
+  ciphertext = decoded_password
+  cipher.update(ciphertext) + cipher.finish
+end
+
+def parse_wifi_config
+  wifi_config_path = ENV['WIFI_CONFIG_PATH'] || ENV_DEFAULT_WIFI_CONFIG_PATH
+
+  unless File.file?(wifi_config_path)
+    puts "File #{wifi_config_path} does not exist"
+    return
+  end
+
+  config = File.open(wifi_config_path, "r") do |f|
+    YAML.load(f.read.to_s)
+  end
+
+  begin
+    ssid = config['wifi']['ssid']
+    encoded_password = config['wifi']['encoded_password']
+  rescue
+    puts "Invalid configuration file"
+    return
+  end
+
+  return ssid, encoded_password
+end
+
+def setup_wifi
+  ssid, encoded_password = parse_wifi_config
+  return if ssid.nil? || encoded_password.nil?
+
+  ESP32::WiFi.init
+  ESP32::WiFi.connect_timeout(ssid, encoded_password, ESP32::Auth::WPA2_AES_PSK, 10000)
+end
 
 # Setup flash disk
 begin
@@ -18,6 +68,8 @@ rescue => e
 end
 
 begin
+  setup_wifi
+
   if File.exist?("/home/app.mrb")
     puts "Loading app.mrb"
     load "/home/app.mrb"
