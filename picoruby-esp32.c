b5e2d301a6b78c9cc69fefcfa33e1685db77119f
Yuhei Okazaki <okazaki@fusic.co.jp>
2025-12-31 17:53:37 +0900

    MicroRuby on ESP32.

--- picoruby-esp32.c
+++ picoruby-esp32.c
@@ -2,7 +2,12 @@
 #include <nvs_flash.h>
 #include "picoruby.h"
 
+#if defined(PICORB_VM_MRUBYC)
 #include <mrubyc.h>
+#elif defined(PICORB_VM_MRUBY)
+#include "hal.h" // in picoruby-machine
+#endif
+
 #include "mrb/main_task.c"
 
 #ifndef HEAP_SIZE
@@ -16,6 +21,10 @@
 #endif
 
 static uint8_t heap_pool[HEAP_SIZE];
+
+#if defined(PICORB_VM_MRUBY)
+mrb_state *global_mrb = NULL;
+#endif
 
 void
 initialize_nvs(void)
@@ -32,6 +41,8 @@ void
 picoruby_esp32(void)
 {
   initialize_nvs();
+
+#if defined(PICORB_VM_MRUBYC)
   mrbc_init(heap_pool, HEAP_SIZE);
 
   mrbc_tcb *main_tcb = mrbc_create_task(main_task, 0);
@@ -40,4 +51,24 @@ picoruby_esp32(void)
 
   picoruby_init_require(vm);
   mrbc_run();
+#elif defined(PICORB_VM_MRUBY)
+  mrb_state *mrb = mrb_open_with_custom_alloc(heap_pool, HEAP_SIZE);
+  global_mrb = mrb;
+  mrc_irep *irep = mrb_read_irep(mrb, main_task);
+  mrc_ccontext *cc = mrc_ccontext_new(mrb);
+  mrb_value name = mrb_str_new_lit(mrb, "R2P2");
+  mrb_value task = mrc_create_task(cc, irep, name, mrb_nil_value(), mrb_obj_value(mrb->top_self));
+  if (mrb_nil_p(task)) {
+    const char *msg = "mrbc_create_task failed\n";
+    hal_write(1, msg, strlen(msg));
+  }
+  else {
+    mrb_tasks_run(mrb);
+  }
+  if (mrb->exc) {
+    mrb_print_error(mrb);
+  }
+  mrb_close(mrb);
+  mrc_ccontext_free(cc);
+#endif
 }
