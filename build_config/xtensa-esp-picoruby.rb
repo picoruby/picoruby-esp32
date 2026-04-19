efdc17af6993daa02a72b3c753e0b92e1d683b64
Yuhei Okazaki <okazaki@fusic.co.jp>
2026-04-19 14:07:43 +0900

    Connecting to Wi-Fi in PicoRuby on the ESP32.

--- build_config/xtensa-esp-picoruby.rb
+++ build_config/xtensa-esp-picoruby.rb
@@ -24,6 +24,7 @@ MRuby::CrossBuild.new('esp32-picoruby') do |conf|
   conf.cc.defines << 'USE_FAT_FLASH_DISK'
   conf.cc.defines << 'NDEBUG'
   conf.cc.defines << 'ESP32_PLATFORM'
+  conf.cc.defines << "CONFIG_ESP_WIFI_ENABLED" unless ENV['CONFIG_ESP_WIFI_ENABLED'].to_s.empty?
 
   if ENV['PICORB_DEBUG']
     conf.cc.defines << 'ESTALLOC_DEBUG'
@@ -41,8 +42,6 @@ MRuby::CrossBuild.new('esp32-picoruby') do |conf|
   conf.gem gemdir: '../picoruby/mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-error'
   conf.gem gemdir: '../picoruby/mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-sprintf'
   conf.gem gemdir: '../picoruby/mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-math'
-  conf.gem core: 'picoruby-esp32'
-  conf.gembox 'shell'
 
   # stdlib
   conf.gem core: 'picoruby-rng'
@@ -59,7 +58,12 @@ MRuby::CrossBuild.new('esp32-picoruby') do |conf|
 
   # others
   # conf.gem core: 'picoruby-rmt'
+  conf.gem core: 'picoruby-esp32'
   conf.gem core: 'picoruby-mbedtls'
   conf.gem core: 'picoruby-socket'
+  conf.gem core: 'picoruby-network'
   # conf.gem core: 'picoruby-adafruit_sk6812'
+
+  # shell
+  conf.gembox 'shell'
 end
